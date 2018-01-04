684d2d1f11ab4c6fcecceacf084a9c6408230906
yaoxuanzhi <yaoxuanzhi@outlook.com>
2018-01-04 14:37:14 +0800

    add Cliplus library

---
+++ Utilities/Cliplus/Cliplus.cpp
@@ -0,0 +1,192 @@
+﻿#include "Cliplus.h"
+#include <stdio.h>
+#include <process.h>
+#include <windows.h>
+#pragma warning(disable:4996)
+
+/*****************************************************************************
+* [windows 控制台程序 响应 Ctrl+C](http://blog.csdn.net/shuiba1122/article/details/40950671)
+* [控制台程序输入CTRL+C后弹出异常的原因](http://blog.csdn.net/oilcode/article/details/8886806)
+*****************************************************************************/
+
+BOOL CtrlHandler(DWORD fdwCtrlType)
+{
+    switch (fdwCtrlType)
+    {
+        // Handle the CTRL-C signal.   
+    case CTRL_C_EVENT:
+        fputs("Ctrl-C event\n\n", stdout);
+        //Beep( 750, 300 );   
+        return(TRUE);
+        // CTRL-CLOSE: confirm that the user wants to exit.   
+    case CTRL_CLOSE_EVENT:
+        //Beep( 600, 200 );   
+        fputs("Ctrl-Close event\n\n", stdout);
+        return(TRUE);
+        // Pass other signals to the next handler.   
+    case CTRL_BREAK_EVENT:
+        //Beep( 900, 200 );   
+        fputs("Ctrl-Break event\n\n", stdout);
+        return FALSE;
+    case CTRL_LOGOFF_EVENT:
+        //Beep( 1000, 200 );   
+        fputs("Ctrl-Logoff event\n\n", stdout);
+        return FALSE;
+    case CTRL_SHUTDOWN_EVENT:
+        //Beep( 750, 500 );   
+        fputs("Ctrl-Shutdown event\n\n", stdout);
+        return FALSE;
+    default:
+        return FALSE;
+    }
+}
+
+namespace CliplusNsp {
+    /** 
+    * @brief: 获得控制台窗口句柄
+    * @return HWND CLI窗口句柄
+    * @note   
+    * 
+    */
+    HWND GetConsoleWnd()
+    {
+        char szWndTitle[256] = { 0 };
+        GetConsoleTitleA(szWndTitle, 256);
+        return FindWindowA("ConsoleWindowClass", szWndTitle);
+    }
+
+    /**
+    * @brief: 设置窗口置顶
+    * @param[in] bool  bIsAlwaysTop  如果为true则置顶，反之，取消置顶
+    * @return 无
+    * @note
+    *
+    */
+    void SetAlwaysOnTop(bool bIsAlwaysTop = false)
+    {
+        HWND hWndInsertAfter = bIsAlwaysTop ? HWND_TOPMOST : HWND_NOTOPMOST;
+        SetWindowPos(GetConsoleWnd(), hWndInsertAfter, 0, 0, 0, 0, SWP_NOACTIVATE | SWP_SHOWWINDOW | SWP_NOMOVE | SWP_NOSIZE);
+    }
+
+    unsigned int __stdcall ThreadCliLoopFunc(void* lpParam)
+    {
+        CCliplus *pThis = (CCliplus*)lpParam;
+        if (NULL != pThis) {
+            pThis->OnMessageLoop();
+        }
+        return 0;
+    }
+}
+
+CCliplus::CCliplus() :m_emSubSys(EM_CONSOLE), m_bLoop(false), m_bAcceptCmd(true)
+{
+}
+
+void CCliplus::ShowHelpTips()
+{
+    fputs("Cliplus used for GUI/CLI Application on Windows\n", stdout);
+}
+
+void CCliplus::ShowQuitTip()
+{
+    fputs("Press Any Key To Quit...\n", stdout);
+}
+
+void CCliplus::SetSubSysType(EM_SUBSYSTEM subSysType)
+{
+    m_emSubSys = subSysType;
+}
+
+void CCliplus::AcceptCommand(char *szCmd)
+{
+    fputs(szCmd, stdout);
+}
+
+void CCliplus::Close()
+{
+    m_bLoop = false;
+
+    //在控制台上模拟按下Enter键
+    ::PostMessage(CliplusNsp::GetConsoleWnd(), WM_KEYDOWN, VK_RETURN, NULL);
+}
+
+bool CCliplus::OnQuit()
+{
+    m_bLoop = false;
+    ShowQuitTip();
+    fgetchar();
+    return true;
+}
+
+void CCliplus::Open()
+{
+    if (m_bLoop)
+        return;
+    switch (m_emSubSys)
+    {
+    case EM_WINDOWS:
+    {
+        AllocConsole();
+        freopen("CONIN$", "r", stdin);
+        freopen("CONOUT$", "w", stdout);
+        freopen("CONOUT$", "w", stderr);
+        _beginthreadex(NULL, 0, CliplusNsp::ThreadCliLoopFunc, this, 0, NULL);
+    }; break;
+    case EM_CONSOLE:
+    {
+        OnMessageLoop();
+    }; break;
+    default:
+        break;
+    }
+}
+
+unsigned int CCliplus::OnMessageLoop()
+{
+    // 重置Ctrl相关的信号作用
+    if (SetConsoleCtrlHandler((PHANDLER_ROUTINE)CtrlHandler, TRUE))
+    {
+        const int nCmdLen = 512;
+        m_bLoop = true;
+        while (m_bLoop)
+        {
+            if (!m_bAcceptCmd) {
+                Sleep(10);
+                continue;
+            }
+            char szCmdBuff[nCmdLen] = { 0 };
+            fgets(szCmdBuff, sizeof(char) * nCmdLen, stdin);
+
+            if (*szCmdBuff == 0) {
+                Sleep(500);
+                if (OnQuit())
+                    break;
+            }
+
+            szCmdBuff[strlen(szCmdBuff) - 1] = '\0'; //按下Enter之后多出来的"\n"
+
+            if (strncmp(szCmdBuff, "\\q", 2) == 0 ||
+                strncmp(szCmdBuff, "exit", 4) == 0 ||
+                strncmp(szCmdBuff, "quit", 4) == 0) {
+                OnQuit();
+            }
+            else if (strncmp(szCmdBuff, "settop", 6) == 0) {
+                CliplusNsp::SetAlwaysOnTop(true);
+            }
+            else if (strncmp(szCmdBuff, "notop", 5) == 0) {
+                CliplusNsp::SetAlwaysOnTop(false);
+            }
+            else if (strncmp(szCmdBuff, "-h", 2) == 0 ||
+                strncmp(szCmdBuff, "/?", 2) == 0) {
+                ShowHelpTips();
+            }
+            else {
+                AcceptCommand(szCmdBuff);
+            }
+        }
+    }
+
+    if (m_emSubSys == EM_WINDOWS)
+        FreeConsole();
+    return 0;
+}
---
+++ Utilities/Cliplus/Cliplus.h
@@ -0,0 +1,150 @@
+﻿/*****************************************************************************
+*  @file     Cliplus.h
+*  @brief    控制台辅助类
+*  @note
+*  此类库仅支持Windows，要想让其适配Linux/Unix系统请自行修改><
+*
+*****************************************************************************/
+#ifndef __CCLIPLUS_H
+#define __CCLIPLUS_H
+
+/** @brief 子系统类型 */
+enum EM_SUBSYSTEM
+{
+    EM_WINDOWS, /**< 窗口程序 */
+    EM_CONSOLE, /**< 控制台程序 */
+};
+
+/**
+* @class 
+* @brief 控制台辅助类库
+* @note   
+* 支持GUI和CLI程序（Windows）
+*/
+class CCliplus{
+private:
+	bool m_bLoop;/**< 循环控制标记 */
+	bool m_bAcceptCmd;/**< 是否接受外部指令 */
+	EM_SUBSYSTEM m_emSubSys;/**< 子系统类型 */
+public:
+	CCliplus();
+
+	/** 
+	* @brief: 关闭CLI窗口
+	* @return 无
+	* @note   
+	* 
+	*/
+	void Close();
+
+	/** 
+	* @brief: 打开CLI窗口
+	* @return 无
+	* @note   
+	* 
+	*/
+	void Open();
+
+    /** 
+    * @brief: 设置外部输入
+    * @param[in] bool  bIsCan  如果为true，则支持输入，反之无法输入
+    * @return 无
+    * @note   
+    * 
+    */
+    void SetAcceptCmd(bool bIsCanAccept)
+    {
+        m_bAcceptCmd = bIsCanAccept;
+    }
+
+public:
+    virtual unsigned int OnMessageLoop();
+
+protected:
+    /** 
+    * @brief: 设置子系统类型
+    * @param[in] EM_SUBSYSTEM  subSysType  子系统类型
+    * @return 无
+    * @note   
+    * 
+    */
+    void SetSubSysType(EM_SUBSYSTEM subSysType);
+
+	/** 
+	* @brief: 接收用户输入指令
+	* @param[in] char*  szCmd  字符串指令
+	* @return 无
+	* @note   
+	* 
+	*/
+	virtual void AcceptCommand(char *szCmd);
+
+	/** 
+	* @brief: 打印帮助信息
+	* @return 无
+	* @note   
+	* 
+	*/
+	virtual void ShowHelpTips();
+
+	/** 
+	* @brief: 打印退出信息
+	* @return 无
+	* @note   
+	* 
+	*/
+	virtual void ShowQuitTip();
+
+	/** 
+	* @brief: 退出
+	* @return 无
+	* @note   
+	* 
+	*/
+    virtual bool OnQuit();
+};
+
+#endif
+
+/*********************************调用演示************************************
+#include <stdio.h>
+#include <string.h>
+class CCliHelperEx:public CCliplus{
+public:
+    CCliHelperEx()
+    {
+        SetSubSysType(EM_CONSOLE);
+    }
+protected:
+	void AcceptCommand(char *szCmd)
+    {
+        if (strncmp(szCmd,"run", 8) == 0)
+        {
+            fputs("执行其它脚本代码\n", stdout);
+        }
+        else if (strncmp(szCmd,"stop", 6) == 0)
+        {
+            Close();
+        }
+        else {
+            fprintf(stdout, "%s\n", szCmd);
+        }
+    }
+	void ShowHelpTips()
+	{
+        fputs("这只是一个Demo\n", stdout);
+	}
+
+	void ShowQuitTip()
+	{
+        fputs("按下任意键退出...\n", stdout);
+	}
+};
+
+int main()
+{
+    CCliHelperEx obj;
+    obj.Open();
+    return 0;
+}
+*****************************************************************************/
